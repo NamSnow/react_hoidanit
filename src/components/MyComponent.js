d94a769ac8c7747dc85abb7ecea2af69e2456ecd
NamSnow <phamhoainam08102004@gmail.com>
2026-02-21 10:48:21 +0700

    20. séttate

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,43 @@
 import React from "react";
 
-function MyComponent() {
+class MyComponent extends React.Component {
   // JSX
+  state = {
+    name: "NameA3",
+    address: "Hoi Dan It",
+    age: 26,
+  };
 
-  return <div>MyComponent</div>;
+  handleClick(event) {
+    console.log("Click me");
+    console.log("random ", this.state.name);
+
+    this.setState({
+      name: "Eric",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and I'm {this.state.age}
+        <button
+          onClick={(event) => {
+            this.handleHover(event);
+          }}
+        >
+          Hover me
+        </button>
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
+      </div>
+    );
+  }
 }
 
 export default MyComponent;
