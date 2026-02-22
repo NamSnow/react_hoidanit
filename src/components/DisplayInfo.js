c3ed7a728b33db13c6890319463bd863d67a174a
NamSnow <phamhoainam08102004@gmail.com>
2026-02-23 00:01:58 +0700

    23.

---
+++ src/components/DisplayInfo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class DisplayInfo extends React.Component {
+  render() {
+    const { age, name } = this.props;
+
+    return (
+      <div>
+        <div>My name is {name}</div>
+        <div>My age is {age}</div>
+      </div>
+    );
+  }
+}
+
+export default DisplayInfo;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Userinfor from "./Userinfor";
+import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
   // JSX
 
   render() {
+    const myAge = 50;
+    const myArray = ["ab", "c", "d"];
+
     return (
       <div>
         <Userinfor />
+        <br />
+        <DisplayInfo name="Hoi Dan IT" age="266" />
+
+        <hr />
+
+        <DisplayInfo name="Pham Nam" myArray={myArray} />
       </div>
     );
   }
