508dec2cc3a74f74f3f560d087f99bc0b69071e1
NamSnow <phamhoainam08102004@gmail.com>
2026-02-25 15:39:17 +0700

    38. Up code

--- src/components/js/DisplayInfo.js
+++ src/components/js/DisplayInfo.js
@@ -1,94 +1,6 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../scss/DisplayInfo.scss";
 import logo from "./../../logo.svg";
-
-// export class DisplayInfo extends React.Component {
-//   // constructor(props) {
-//   //   console.log("constructor(props)");
-//   //   super(props);
-//   //   // babel compiler
-//   //   this.state = {
-//   //     isShowListUser: true,
-//   //   };
-//   // }
-
-//   // state = {
-//   //   isShowListUser: true,
-//   // };
-
-//   componentDidMount() {
-//     console.log("componentDidMount");
-
-//     setTimeout(() => {
-//       document.title = "TodoList";
-//     }, 3000);
-//   }
-
-//   componentDidUpdate(prevProps, prevState, snapshot) {
-//     console.log("componentDidUpdate", this.props, prevProps);
-
-//     if (this.props.listUsers !== prevProps.listUsers) {
-//       if (this.props.listUsers.length === 5) {
-//         alert("5 users");
-//       }
-//     }
-//   }
-
-//   // handleShowHide = () => {
-//   //   this.setState({
-//   //     isShowListUser: !this.state.isShowListUser,
-//   //   });
-//   // };
-
-//   render() {
-//     console.log("Render");
-//     const { listUsers } = this.props;
-//     // console.log(listUsers);
-
-//     return (
-//       <div className="display-info-container">
-//         {/* <img src={logo} /> */}
-
-//         {/* <div>
-//           <span
-//             style={{ cursor: "pointer" }}
-//             onClick={() => {
-//               this.handleShowHide();
-//             }}
-//           >
-//             {this.state.isShowListUser === true ? "Hide" : "Show"} list users:
-//           </span>
-//         </div> */}
-
-//         {true && (
-//           <>
-//             {listUsers.map((item, index) => {
-//               //   console.log(item);
-
-//               return (
-//                 <div key={item.id} className={item.age < 18 ? "green" : "red"}>
-//                   <div>
-//                     <div>My name is {item.name}</div>
-//                     <div>My age is {item.age}</div>
-//                   </div>
-
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(item.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfo = (props) => {
   const { listUsers } = props;
@@ -102,6 +14,17 @@ const DisplayInfo = (props) => {
   const handleShowHideListUser = () => {
     setIsShowHideListUser(!isShowHideListUser);
   };
+
+  console.log("Call me render");
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (listUsers.length === 0) {
+        alert("You deleted all the users");
+      }
+    }, 3000);
+    console.log("Call me useEffect");
+  }, [listUsers]);
 
   return (
     <div className="display-info-container">
--- src/components/js/MyComponent.js
+++ src/components/js/MyComponent.js
@@ -2,56 +2,6 @@ import React, { useState } from "react";
 import AddUserinfor from "./AddUserinfor.js";
 import DisplayInfo from "./DisplayInfo.js";
 
-// class MyComponent extends React.Component {
-//   state = {
-//     listUsers: [
-//       { id: 1, name: "Hoi Dan It", age: 30 },
-//       { id: 2, name: "Eric", age: 36 },
-//       { id: 3, name: "HarryPhamDev", age: 10 },
-//       { id: 4, name: "Hoi Dan It", age: 30 },
-//     ],
-//   };
-
-//   handleAddNewUser = (userObj) => {
-//     this.setState({
-//       listUsers: [...this.state.listUsers, userObj],
-//     });
-//   };
-
-//   handleDeleteUser = (userId) => {
-//     let listUserClone = [...this.state.listUsers];
-//     listUserClone = listUserClone.filter((item) => item.id !== userId);
-//     this.setState({
-//       listUsers: listUserClone,
-//     });
-//   };
-
-//   // JSX
-
-//   render() {
-//     const myAge = 50;
-//     const myArray = ["ab", "c", "d"];
-
-//     const test = { name: "Eric", age: 16 };
-
-//     return (
-//       <div>
-//         {JSON.stringify(test)}
-
-//         <AddUserinfor handleAddNewUser={this.handleAddNewUser} />
-//         <br />
-
-//         <br />
-
-//         <DisplayInfo
-//           listUsers={this.state.listUsers}
-//           handleDeleteUser={this.handleDeleteUser}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
 const MyComponent = () => {
   const [listUsers, setListUsers] = useState([
     { id: 1, name: "Hoi Dan It", age: 30 },
