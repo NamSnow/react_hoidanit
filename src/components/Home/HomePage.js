fc6689e4f7f0a8618fc3b251ae88878f3ef2a285
NamSnow <phamhoainam08102004@gmail.com>
2026-02-26 01:59:36 +0700

    52. Design HomePagw

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import videoHomePage from "../../assets/video-homepage.mp4";
 
 const HomePage = () => {
-  return <div>HomePage Component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source type="video/mp4" src={videoHomePage} />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
