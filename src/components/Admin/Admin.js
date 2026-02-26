a86a30a12dc917a900d816eff4e4bac904f37e94
NamSnow <phamhoainam08102004@gmail.com>
2026-02-26 21:51:24 +0700

    54. Design Admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import SideBar from "./SideBar";
+import "./Admin.scss";
+import { FaBars } from "react-icons/fa";
 
 const Admin = () => {
-  return <div>Admin component</div>;
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <SideBar collapsed={collapsed} />
+      </div>
+
+      <div className="admin-content">
+        <FaBars onClick={() => setCollapsed(!collapsed)} />
+        Content goes here
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
