adf970190473a8ec5d4352b4fe344939979367c9
NamSnow <phamhoainam08102004@gmail.com>
2026-03-22 06:17:16 +0700

    #84. Design UI

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
 import _ from "lodash";
+import "./DetailQuiz.scss";
 
 const DetailQuiz = (props) => {
   const params = useParams();
+  const location = useLocation();
   const quizId = params.id;
 
   useEffect(() => {
@@ -40,7 +42,38 @@ const DetailQuiz = (props) => {
     }
   };
 
-  return <div className="detail-quiz-container">DetailQuiz</div>;
+  return (
+    <div className="detail-quiz-container">
+      <div className="left-content">
+        <div className="title">
+          Quiz {quizId}: {location?.state?.quizTitle}
+        </div>
+
+        <hr />
+
+        <div className="q-body">
+          <img />
+        </div>
+
+        <div className="q-content">
+          <div className="question">Question 1: How aer you</div>
+
+          <div className="answer">
+            <div>A. 1233</div>
+            <div>B. 1233</div>
+            <div>C. 1233</div>
+          </div>
+        </div>
+
+        <div className="footer d-flex">
+          <button className="btn btn-primary">Prev</button>
+          <button className="btn btn-secondary">Next</button>
+        </div>
+      </div>
+
+      <div className="right-content">count down</div>
+    </div>
+  );
 };
 
 export default DetailQuiz;
