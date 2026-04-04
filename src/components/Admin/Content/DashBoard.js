2b5217601ff063cc12c965eb6769033f7de463a5
NamSnow <phamhoainam08102004@gmail.com>
2026-04-04 08:17:44 +0700

    #111. api dashboard

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DashBoard.scss";
 import {
   BarChart,
@@ -10,47 +10,47 @@ import {
   Bar,
   ResponsiveContainer,
 } from "recharts";
+import { getOverView } from "../../../services/apiServices";
 
 const DashBoard = (props) => {
-  const [isAnimationActive, setIsAnimationActive] = useState(true);
+  const [dataOverView, setDataOverView] = useState([]);
+  const [dataChart, setDataChart] = useState([]);
 
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-    },
-  ];
+  useEffect(() => {
+    fetchDataOverView();
+  }, []);
+
+  const fetchDataOverView = async () => {
+    let res = await getOverView();
+    if (res && res.EC === 0) {
+      setDataOverView(res.DT);
+      // process chart data
+
+      let Qz = res?.DT?.others?.countQuiz ?? 0;
+      let Qs = res?.DT?.others?.countQuestions ?? 0;
+      let As = res?.DT?.others?.countAnswers ?? 0;
+
+      const data = [
+        {
+          name: "Quizzes",
+          Qz: Qz,
+        },
+        {
+          name: "Questions",
+          Qs: Qs,
+        },
+        {
+          name: "Answers",
+          As: As,
+        },
+      ];
+
+      setDataChart(data);
+    }
+    console.log("res", res);
+  };
+
+  console.log(dataOverView);
 
   return (
     <div className="dashboard-container">
@@ -58,22 +58,67 @@ const DashBoard = (props) => {
 
       <div className="content">
         <div className="c-left">
-          <div className="child">Total Users</div>
-          <div className="child">Total Quizzes</div>
-          <div className="child">Total Questions</div>
-          <div className="child">Total Answers</div>
+          <div className="child">
+            <span className="text-1">Total Users</span>
+            <span className="text-2">
+              {dataOverView &&
+              dataOverView.users &&
+              dataOverView.users.total ? (
+                <>{dataOverView.users.total}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span className="text-1">Total Quizzes</span>
+            <span className="text-2">
+              {dataOverView &&
+              dataOverView.others &&
+              dataOverView.others.countQuiz ? (
+                <>{dataOverView.others.countQuiz}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span className="text-1">Total Questions</span>
+            <span className="text-2">
+              {dataOverView &&
+              dataOverView.others &&
+              dataOverView.others.countQuestion ? (
+                <>{dataOverView.others.countQuestion}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span className="text-1">Total Answers</span>
+            <span className="text-2">
+              {dataOverView &&
+              dataOverView.others &&
+              dataOverView.others.countAnswers ? (
+                <>{dataOverView.others.countAnswers}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
         </div>
 
         <div className="c-right">
-          <ResponsiveContainer width={"100%"} height={"100%"}>
-            <BarChart data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
+          <ResponsiveContainer width={"95%"} height={"100%"}>
+            <BarChart data={dataChart}>
+              {/* <CartesianGrid strokeDasharray="3 3" /> */}
               <XAxis dataKey="name" />
-              <YAxis />
+              {/* <YAxis /> */}
               <Tooltip />
               <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
+              <Bar dataKey="Qz" fill="#8884d8" />
+              <Bar dataKey="Qs" fill="#82ca9d" />
+              <Bar dataKey="As" fill="#fcb12a" />
             </BarChart>
           </ResponsiveContainer>
         </div>
